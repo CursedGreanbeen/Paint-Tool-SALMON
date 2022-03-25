6cddbdb0d27c53c72e6810f7d37938d366124dff
CursedGreanbeen <101968163+CursedGreanbeen@users.noreply.github.com>
2022-03-25 19:54:20 +0500

    App is working again; added new cursor
    Launch without error messages. Eraser is functional now and the curser is switched on circle while erasing. Font is bold for group titles. Gogol is removed (for this moment). Menu has submenus for Canvas button, but they don;t do anything yet (though the button functions are not empty). Need to be fixed

--- Paint Tool THING.py
+++ Paint Tool THING.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageGrab
 from tkinter.colorchooser import askcolor
 
 
@@ -11,18 +11,59 @@ def choose_color():
 def file():
     pass
 
+def cnv_size():
+    pass
+
+def cnv_bg():
+    global bg_color, canvasPane
+    bg_color = askcolor('Red')
+    cnv.config(bg=bg_color)
+
+# tools
+def erase():
+    global color, bg_color, canvasPane
+    color = bg_color
+    canvasPane.config(cursor='circle')
+
+
+def input_text():
+    root = Tk()
+    root.title('Text')
+    root.geometry('200x200')
+
+    enter_text = Entry(root)
+    enter_text.place(relx=.5, rely=.1, anchor="c")
+
+    def get_text():
+        global cnv
+        message = StringVar()
+        cnv.create_text(150, 400, text=message.get(),
+                        justify=CENTER, font=("Times", "40"))
+
+    but = Button(root, text='OK', command=get_text)
+    but.place(relx=.5, rely=.3, anchor='c')
+
+    root.mainloop()
+
+
+def get_color():
+    pass
+
 # brush settings
 def choose_recBrush():
-    global brushape, shape
+    global brushape, shape, canvasPane
     brushape, shape = 'R', ''
+    canvasPane.config(cursor='cross')
 
 def choose_ovalBrush():
-    global brushape, shape
+    global brushape, shape, canvasPane
     brushape, shape = 'O', ''
+    canvasPane.config(cursor='cross')
 
 def choose_polyBrush():
-    global brushape, shape
+    global brushape, shape, canvasPane
     brushape, shape = 'P', ''
+    canvasPane.config(cursor='cross')
 
 def brush(event):
     global brushape
@@ -63,13 +104,13 @@ def draw_shape(event):
     elif shape == 'L':
         cnv.create_line(xy, fill=color[1])
 
-color = (0, 'Black')
+color, bg_color = (0, 'Black'), (0, 'White')
 brushape, shape = 'O', ''
 
 # window
 window = Tk()
 window.geometry('600x500')
-window.title('Paint Tool SASAI')
+window.title('Paint Tool THING')
 
 # working panes
 toolbarPane = Frame(window)
@@ -81,45 +122,52 @@ imagePane.pack()
 brushPane.pack()
 canvasPane.pack()
 
-
-# just a pic
-gogol = ImageTk.PhotoImage(Image.open("C:/Users/1/Desktop/картинки/стикеры в телегу/Снимок.PNG"))
-just_pic = Label(imagePane, image=gogol)
-just_pic.pack(side=TOP)
-
-
 # canvas
-w = 'white'
-cnv = Canvas(canvasPane, width=500, height=500, bg=w)
+wd, ht = 500, 500
+cnv = Canvas(canvasPane, width=wd, height=ht, bg=bg_color[1])
 cnv.pack()
 
 # menu
 menu_bar = Menu(window)
+cnv_manager = Menu(menu_bar)
 window.config(menu=menu_bar)
 menu_bar.add_command(label='File', command=file)
+menu_bar.add_cascade(label='Canvas', menu=cnv_manager)
+cnv_manager.add_command(label='Size', command=cnv_size)
+cnv_manager.add_command(label='Background', command=cnv_bg)
 menu_bar.add_command(label='Reset', command=lambda : cnv.delete('all'))
 
+# tool buttons
+tools_label = Label(toolbarPane, text='\nTools', font=("Arial", 10, "bold"), width=15)
+eraser_btn = Button(toolbarPane, text='Eraser', width=15, command=erase)
+text_button = Button(toolbarPane, text='Text', width=15, command=input_text)
+pipette_btn = Button(toolbarPane, text='Pipette', width=15, command=get_color)
+
 # brush buttons
-brushes_lbl = Label(brushPane, text='Brushes', width=15)
-rec_brush_btn = Button(brushPane, text='☐', width=4, command=choose_recBrush)
-oval_brush_button = Button(brushPane, text='〇', width=4, command=choose_ovalBrush)
-poly_brush_btn = Button(brushPane, text='///', width=4, command=choose_polyBrush)
+brushes_lbl = Label(brushPane, text='Brushes', font=("Arial", 10, "bold"), width=15)
+rec_brush_btn = Button(brushPane, text='☐', width=5, command=choose_recBrush)
+oval_brush_button = Button(brushPane, text='〇', width=5, command=choose_ovalBrush)
+poly_brush_btn = Button(brushPane, text='///', width=5, command=choose_polyBrush)
 
 # shape buttons
-shapes_lbl = Label(toolbarPane, text='\nShapes', width=15)
+shapes_lbl = Label(toolbarPane, text='\nShapes', font=("Arial", 10, "bold"), width=15)
 rec_btn = Button(toolbarPane, text='Rectangle', width=15, command=choose_rectangle)
 oval_btn = Button(toolbarPane, text='Oval', width=15, command=choose_oval)
 line_btn = Button(toolbarPane, text='Line', width=15, command=choose_line)
 
-# tool buttons
+# parameter buttons
 col_btn = Button(toolbarPane, text='Color', width=15, command=choose_color)
-height_lbl = Label(toolbarPane, text='\nHeight', width=15)
+height_lbl = Label(toolbarPane, text='\nHeight', font=("Arial", 10, "bold"), width=15)
 height_scl = Scale(toolbarPane, from_=0, to=100, width=15, orient=HORIZONTAL)
-width_lbl = Label(toolbarPane, text='Width', width=15)
+width_lbl = Label(toolbarPane, text='\nWidth', font=("Arial", 10, "bold"), width=15)
 width_scl = Scale(toolbarPane, from_=0, to=100, width=15, orient=HORIZONTAL)
 
 # packpackpack
-just_pic.pack()
+tools_label.pack()
+eraser_btn.pack()
+text_button.pack()
+pipette_btn.pack()
+
 brushes_lbl.pack()
 rec_brush_btn.pack(side=LEFT)
 oval_brush_button.pack(side=LEFT)
